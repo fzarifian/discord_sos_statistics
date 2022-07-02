bf4e2097f27666ce2d1c422bff7fddc742392b2e
Fabien ZARIFIAN <fabien.zarifian@nuevolia.fr>
2022-07-02 13:46:27 +0200

    Add base structure for bot

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python3 sos_statistics.py
---
+++ sos_statistics.py
@@ -0,0 +1,49 @@
+import os
+import discord
+import logging
+from discord.ext import commands
+import asyncio
+import asyncpg
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+
+async def run():
+    description = "A bot written in Python that uses asyncpg to connect to a postgreSQL database."
+
+    # NOTE: 127.0.0.1 is the loopback address. If your db is running on the same machine as the code, this address will work
+    db = await asyncpg.create_pool(os.environ.get("DATABASE_URL"))
+
+    # Example create table code, you'll probably change it to suit you
+    await db.execute("CREATE TABLE IF NOT EXISTS users(id bigint PRIMARY KEY, data text);")
+
+    bot = Bot(description=description, db=db, logging=logging)
+    try:
+        await bot.start(os.environ.get("DISCORD_TOKEN"))
+    except KeyboardInterrupt:
+        await db.close()
+        await bot.logout()
+
+class Bot(commands.Bot):
+    def __init__(self, **kwargs):
+        super().__init__(
+            description=kwargs.pop("description"),
+            command_prefix="?"
+        )
+
+        self.db = kwargs.pop("db")
+
+    async def on_ready(self):
+        # .format() is for the lazy people who aren't on 3.6+
+        print("Username: {0}\nID: {0.id}".format(self.user))
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.reply('!hello', mention_author=True)
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
