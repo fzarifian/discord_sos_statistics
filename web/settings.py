4bdbefcbad28a60e5a7bf85fe9031d9d23c21828
Fabien ZARIFIAN <fabien.zarifian@nuevolia.fr>
2022-07-02 17:34:58 +0200

    full path for static

--- web/settings.py
+++ web/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
 import dj_database_url
 from pathlib import Path
 
@@ -119,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
